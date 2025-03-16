f2dcb1da1fa4cfdb6d748a1bfc1688179f0523b4
iBadVan <ireano@unsa.edu.pe>
2025-03-16 18:47:29 -0500

    Avance coordenadas

--- PyPooAct1/Coordenada.java
+++ PyPooAct1/Coordenada.java
@@ -1,5 +1,41 @@
 package PyPooAct1;
 
 public class Coordenada {
+    private int x;
+    private int y;
+
+    public Coordenada(){
+        this.x=0;
+        this.y=0;
+    }
+
+    public Coordenada(int nx, int ny){
+        this.x=nx;
+        this.y=ny;
+    }
+
+    public Coordenada( Coordenada co){
+        this.x=co.x;
+        this.y=co.y;
+    }
+
+    public int getX(){
+        return this.x;
+    }
+
+    public void setX(int nx){
+        this.x=nx;
+    }
+
+    public int getY(){
+        return this.y;
+    }
+
+    public void setY(int ny){
+        this.y=ny;
+    }
+
     
+
+
 }
