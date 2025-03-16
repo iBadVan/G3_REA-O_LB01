f4c2333ec143069b29be956f9be873836ef73e5a
iBadVan <ireano@unsa.edu.pe>
2025-03-16 18:16:58 -0500

    Update Prueba.java
    es la segunda prueba ga

--- Prueba.java
+++ Prueba.java
@@ -1,5 +1,7 @@
 public class Prueba {
     public static void main(String[] Args){
         System.out.println("HOla Peru lindo");
+
+        System.out.println("Este sera uns egundo commit xd");
     }
 }
