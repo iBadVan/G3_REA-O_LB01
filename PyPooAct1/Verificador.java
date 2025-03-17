eba6ac792920b322c988763b236d10628d6d846d
iBadVan <ireano@unsa.edu.pe>
2025-03-16 19:12:12 -0500

    Fija para SobrePos

--- PyPooAct1/Verificador.java
+++ PyPooAct1/Verificador.java
@@ -1,5 +1,10 @@
 package PyPooAct1;
 
 public class Verificador {
-    
+    public static boolean esSobrePos(Rectangulo r1, Rectangulo r2){
+        boolean Esquina1= r1.getEsquina1().getX()==r2.getEsquina1().getX() && r1.getEsquina1().getY() == r2.getEsquina1().getY();
+        
+        
+        return
+    }
 }
