6a326b180878e8dca1f9973674d70029b5e9f025
iBadVan <ireano@unsa.edu.pe>
2025-03-16 19:04:31 -0500

    Clse rectangulo

--- PyPooAct1/Rectangulo.java
+++ PyPooAct1/Rectangulo.java
@@ -9,6 +9,23 @@ public class Rectangulo {
         this.esquina1=new Coordenada(co2);
     }
 
+    public Coordenada getEsquina1() { 
+        return esquina1; 
+    }
 
-    
+    public void setEsquina1(Coordenada co) { 
+        this.esquina1 = new Coordenada(co); 
+    }
+
+    public Coordenada getEsquina2() { 
+        return esquina2; 
+    }
+    public void setEsquina2(Coordenada co) { 
+        this.esquina2 = new Coordenada(co); 
+    }
+
+    public String toString() {
+        return "Rectangulo [" + esquina1 + ", " + esquina2 + "]";
+    }
+
 }
