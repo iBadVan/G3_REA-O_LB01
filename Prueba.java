07aee5ec26f1a90d47c6e0ed6681799b6958ec28
iBadVan <ireano@unsa.edu.pe>
2025-03-16 19:00:37 -0500

    cambios

--- Prueba.java
+++ Prueba.java
@@ -1,7 +1,9 @@
 public class Prueba {
+    /*  
     public static void main(String[] Args){
         System.out.println("HOla Peru lindo");
 
         System.out.println("Este sera uns egundo commit xd");
     }
+    */
 }
--- PyPooAct1/Rectangulo.java
+++ PyPooAct1/Rectangulo.java
@@ -1,5 +1,14 @@
 package PyPooAct1;
 
 public class Rectangulo {
+    private Coordenada esquina1;
+    private Coordenada esquina2;
+
+    public Rectangulo(Coordenada co1, Coordenada co2){
+        this.esquina1=new Coordenada(co1);
+        this.esquina1=new Coordenada(co2);
+    }
+
+
     
 }
