f3dcde12a1e5dbf2d3e3691b4211f5a376d1f14a
iBadVan <ireano@unsa.edu.pe>
2025-03-19 21:30:55 -0500

    Update Verificador.java

--- PyPooAct1/Verificador.java
+++ PyPooAct1/Verificador.java
@@ -2,11 +2,20 @@ package PyPooAct1;
 
 public class Verificador {
     public static boolean esSobrePos(Rectangulo r1, Rectangulo r2){
+        /*
         boolean verificarEsquinasMAYORES= r1.getEsquina1().getX()<=r2.getEsquina2().getX() && r1.getEsquina1().getY()<=r2.getEsquina2().getY();
 
         boolean verificarEsquinasMENORES= r2.getEsquina1().getX()<=r1.getEsquina2().getX() && r2.getEsquina1().getY()<=r1.getEsquina2().getY();
 
         return verificarEsquinasMENORES && verificarEsquinasMAYORES;
+        NO funciona si es que uno esta dentro y en algunos casos de si esta al revés.
+        */
+
+        boolean noSeparadosX = !(r1.getEsquina2().getX()<=r2.getEsquina1().getX() || r1.getEsquina1().getX()>=r2.getEsquina2().getX());   
+    
+        boolean noSeparadosY = !(r1.getEsquina2().getY() <= r2.getEsquina1().getY() || r1.getEsquina1().getY() >= r2.getEsquina2().getY());
+        
+        return noSeparadosX && noSeparadosY;
     }
 
     public static boolean esJunto(Rectangulo r1, Rectangulo r2){
