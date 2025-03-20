9c204fddfe5439d744f50fad56a8a47b184a04e5
iBadVan <ireano@unsa.edu.pe>
2025-03-20 15:43:17 -0500

    Update ContainerRect.java

--- PyPooEje1/ContainerRect.java
+++ PyPooEje1/ContainerRect.java
@@ -15,6 +15,40 @@ public class ContainerRect {
         this.areas= new double[tamano];
         int numRect=0;
     }    
+
+    public void anadirRectangulo(Rectangulo rectangulito){
+        if (numRect<n){
+            this.arregloRectangulos[numRect]=rectangulito;
+            this.distancias[numRect]=rectangulito.getEsquina1().distancia(rectangulito.getEsquina2());
+            this.areas[numRect]=rectangulito.calculoArea();
+            numRect++;
+        } else {
+            System.out.println("NO HAY ESPACIO");
+        }
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder textoF = new StringBuilder();
+        
+        for (int i=0; i<numRect; i++){
+
+            Coordenada esquina1=new Coordenada(arregloRectangulos[i].getEsquina1()); 
+            Coordenada esquina2=new Coordenada(arregloRectangulos[i].getEsquina2()); 
+
+            String coordenadas="["+esquina1.getX()+", "+esquina1.getY()+"] " + " ["+esquina2.getX()+", "+esquina2.getY()+"]" ;
+
+            textoF.append( "|| #Rectagngulo: "+ (i+1) );
+            textoF.append( " || Coordenadas: "+coordenadas );
+            textoF.append( " || Distancia: " + distancias[i] );
+            textoF.append( " || Área: "+ areas[i] );
+              
+
+        }
+        return textoF.toString();
+        
+    }
+
 }
 
 
