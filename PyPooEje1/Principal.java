bbcea7be9d21f7053f546689277ccda8d396468d
iBadVan <ireano@unsa.edu.pe>
2025-03-20 15:53:58 -0500

    añadimos las nuevas funcionalidades al main

--- PyPooEje1/Principal.java
+++ PyPooEje1/Principal.java
@@ -6,6 +6,11 @@ public class Principal {
     public static void main(String[] args) {
         Scanner scan= new Scanner(System.in);
         
+        //CONTENEDOL
+
+        ContainerRect contenedor = new ContainerRect(5);
+
+        //PRIMER RECT
         System.out.println("INGRESE LAS COORDENADAS DE LA ESQUINA 1 PARA R1 ");
         System.out.println("ESQUINA1 X de R1: ");
         double r1x1=scan.nextDouble();
@@ -19,7 +24,9 @@ public class Principal {
         double r1y2=scan.nextDouble();
         System.out.println(r1x2+" "+r1y2);
         Rectangulo Rectangulo1= new Rectangulo(new Coordenada(r1x1,r1y1), new Coordenada(r1x2,r1y2));
+        contenedor.anadirRectangulo(Rectangulo1);
 
+        //SEGUNDO RECT
         System.out.println("INGRESE LAS COORDENADAS DE LA ESQUINA 1 PARA R2 ");
         System.out.println("ESQUINA1 X de R2: ");
         double r2x1=scan.nextDouble();
@@ -33,8 +40,11 @@ public class Principal {
         double r2y2=scan.nextDouble();
         System.out.println(r2x2+" "+r2y2);
         Rectangulo Rectangulo2= new Rectangulo(new Coordenada(r2x1,r2y1), new Coordenada(r2x2,r2y2));
+        contenedor.anadirRectangulo(Rectangulo2);
 
-        System.out.println("Se crearon los rectangulos: R1: "+Rectangulo1+" ; R2: "+Rectangulo2);
+        //USAMOS EL METODO QUE SOBREESCRIBIMOS EN LA NUEVA CLASE CONTAINER
+        System.out.println("Se crearon los rectángulos: ");
+        System.out.println(contenedor.toString());
         
         System.out.println("Rectángulo A = " + Rectangulo1);
         System.out.println("Rectángulo B = " + Rectangulo2);
