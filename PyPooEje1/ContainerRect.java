d5f00bdb291a9dd2ce372167eacbe6f7c6a517ea
iBadVan <ireano@unsa.edu.pe>
2025-03-20 14:26:35 -0500

    Update ContainerRect.java

--- PyPooEje1/ContainerRect.java
+++ PyPooEje1/ContainerRect.java
@@ -6,13 +6,17 @@ public class ContainerRect {
     private double[] distancias;
     private double[] areas;
     private int n;
+    private int numRect; //contadol de ractangulos hasta ciertos tiempos;
 
     public ContainerRect (int tamano){
         this.n=tamano;
-        this.arregloRectangulos=new arregloRectangulos[tamano];
-
-
+        this.arregloRectangulos=new Rectangulo[tamano];
+        this.distancias= new double[tamano];
+        this.areas= new double[tamano];
+        int numRect=0;
     }
+
+    
 
 
     
