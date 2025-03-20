4cca6525ec73c087129cd28860e64e84f126538c
iBadVan <ireano@unsa.edu.pe>
2025-03-20 14:25:21 -0500

    constructor y atributos de containerRect

--- PyPooEje1/ContainerRect.java
+++ PyPooEje1/ContainerRect.java
@@ -3,6 +3,17 @@ package PyPooEje1;
 public class ContainerRect {
 
     private Rectangulo[] arregloRectangulos;
-    
+    private double[] distancias;
+    private double[] areas;
+    private int n;
+
+    public ContainerRect (int tamano){
+        this.n=tamano;
+        this.arregloRectangulos=new arregloRectangulos[tamano];
+
+
+    }
+
+
     
 }
